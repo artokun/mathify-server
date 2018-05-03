37378b8213167f311c00078585d9ee2c8cca3af5
artokun <art.longbottom.jr@gmail.com>
2018-05-02 20:18:36 -0700

    initial commit

---
+++ tests/math.spec.js
@@ -0,0 +1,14 @@
+const server = require('../index.js');
+const { mathify } = require('./helpers/mathHelper');
+
+process.env.NODE_ENV = 'test';
+
+describe('Can do maths', () => {
+  test('10 * 5 / 2', async () => {
+    expect(await mathify(10, 5, 2)).toEqual(25);
+  });
+});
+
+afterAll(async () => {
+  await server.close();
+});
